6142aa33168ac0f441c7bc015aff2c0695fb01e8
Karim <karimatef65@gmail.com>
2020-11-17 02:04:55 +0200

    fix stack over flow error

--- src/algorithms/Utility.java
+++ src/algorithms/Utility.java
@@ -2,13 +2,19 @@ package algorithms;
 
 import representaation.State;
 
+import java.util.Stack;
+
 public class Utility {
 	
 	public void backtrack(State state) {
-		if (state == null)
-			return;
-		backtrack(state.getParent());
-		print(state);
+		Stack<State> stack = new Stack<>();
+		while (state != null) {
+			stack.push(state);
+			state = state.getParent();
+		}
+		while (!stack.isEmpty()) {
+			print(stack.pop());
+		}
 	}
 	public void print(State state) {
 		for(int row = 0; row < 3; row++) {
